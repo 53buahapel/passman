ea777467b4af3a0fcb5ff4c9348882eaf7620990
Rafi Nur Ardiansyah <rafimaster9@gmail.com>
2025-04-05 19:06:40 +0700

    feat: Implement password visibility toggle and copy functionality
    - Added password visibility toggle feature for both card and table views.
    - Enhanced copy functionality for email and password fields with visual feedback.
    - Updated SCSS styles for improved UI/UX, including card and button styles.
    - Refactored JavaScript to handle password visibility and copy actions more efficiently.
    - Modified templates to support new password handling and display logic.
    - Ensured user-specific data handling in views for security and privacy.

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include(('auth.urls', 'auth'), namespace='auth')),
     path('management/', include(('management.urls',
-         'management'), namespace='management')),
+        'management'), namespace='management')),
     path('', include(('home.urls', 'home'), namespace='home'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- management/forms.py
+++ management/forms.py
@@ -5,7 +5,7 @@ from .models import Account
 class ManagementCreateForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = '__all__'
+        fields = ['image_site', 'site_name', 'email', 'username', 'password']
         widgets = {
             'site_name': forms.TextInput(attrs={'placeholder': 'Site name'}),
             'email': forms.TextInput(attrs={'placeholder': 'Email'}),
